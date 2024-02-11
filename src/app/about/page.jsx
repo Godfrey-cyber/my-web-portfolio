982ffdd596d10b24b139c36e15511a7f51245128
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-02-11 21:35:34 +0300

    first-commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,14 +16,14 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image src="/hero.png" alt="" fill className="object-contain" />
         </div>
-         {/* TEXT CONTAINER */}
+        {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
-          <h1 className="text-4xl md:text-6xl font-bold">
+          <h1 className="text-4xl md:text-6xl font-bold text-gray-700">
             Crafting Digital Experiences, Designing Tomorrow.
           </h1>
           {/* DESC */}
-          <p className="md:text-xl">
+          <p className="md:text-xl text-gray-600">
             Welcome to my digital canvas, where innovation and creativity
             converge. With a keen eye for aesthetics and a mastery of code, my
             portfolio showcases a diverse collection of projects that reflect my
